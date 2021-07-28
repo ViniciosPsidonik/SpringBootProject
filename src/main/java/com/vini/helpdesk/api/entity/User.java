c27eb888d8ae08ecae9f837c721975ca34825adb
Vinicios Psidonik <vinicios.psidonik@compasso.com.br>
2021-07-28 17:09:10 -0300

    aaa

--- src/main/java/com/vini/helpdesk/api/entity/User.java
+++ src/main/java/com/vini/helpdesk/api/entity/User.java
@@ -1,5 +1,32 @@
 package com.vini.helpdesk.api.entity;
 
-public class User {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
+public class User {
+	
+	@Id
+	private String id;
+	
+	@Indexed
+	private String email;
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
